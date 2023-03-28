3c88e28108a2a6bdf5bfdef5b07d52d8a006fb91
Amber Srivastava <ambersrivastava19@gmail.com>
2023-03-28 17:54:24 +0530

    chap - 7

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,6 @@ import { useRouteError } from "react-router-dom";
 const Error = () => {
 
     const err = useRouteError();
-    console.log(err)
     return (
         <div className="error-page">
             <h1>oops!!</h1>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -18,14 +18,13 @@ const ResaturantMenu = () => {
       `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=${resId}&submitAction=ENTER`
     );
     const json = await data.json();
-    console.log(json.data);
     setRestaurant(json.data);
   }
 
   return !restaurant ? (
     <Shimmer />
   ) : (
-    <div className="restaurant-page\" key={restaurant.id}>
+    <div className="restaurant-page" key={restaurant.id}>
       <div className="restaurant-info">
         <h1>Restaurnat id: {resId}</h1>
         <h1>Meghana foods</h1>
@@ -40,12 +39,41 @@ const ResaturantMenu = () => {
         <h3>{restaurant?.cards[0]?.card.card?.info?.city}</h3>
         <h3>{restaurant?.cards[0]?.card.card?.info?.cuisines.join(", ")}</h3>
         <h3>{restaurant?.cards[0]?.card.card?.info?.costForTwoMessage}</h3>
+        <h4>
+          {
+            restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[0]
+              ?.card?.card?.vegOnlyDetails?.title
+          }
+        </h4>
       </div>
       <div className="restaurant-menu">
-        
+        {restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
+          ?.slice(2, 10)
+          .map((e, index) => { return (
+            e?.card?.card?.itemCards?.map((b) => {
+              console.log(b)
+              return <li key={b?.card?.info?.id}>{b?.card?.info?.name}</li>
+            })
+          )})}
       </div>
     </div>
   );
 };
 
 export default ResaturantMenu;
+
+// restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
+//   ?.slice(1)
+//   .map((e) => {
+//     console.log(e);
+//     {
+//       e?.card?.itemCards?.map((e) => {
+//         return <h4>{e?.card?.info?.name}</h4>;
+//       });
+//     }
+//   });
+
+
+// e?.card?.card?.itemCards?.map((b) => {
+//   return <h4>{b?.card?.info?.name}</h4>;
+// });
